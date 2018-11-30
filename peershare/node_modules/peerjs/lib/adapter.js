6db3a63018572307f4a33e3916b7db3d4603409b
vsabado <VladS569041923>
2018-11-29 18:13:29 -0600

    Added src and node_modules

---
+++ peershare/node_modules/peerjs/lib/adapter.js
@@ -0,0 +1,8 @@
+require('webrtc-adapter');
+
+module.exports.RTCSessionDescription = window.RTCSessionDescription ||
+	window.mozRTCSessionDescription;
+module.exports.RTCPeerConnection = window.RTCPeerConnection ||
+	window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+module.exports.RTCIceCandidate = window.RTCIceCandidate ||
+	window.mozRTCIceCandidate;
